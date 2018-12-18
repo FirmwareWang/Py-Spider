b5e35d34ab6e9079d2e34d1b658414a7643d3f2a
jinli.wang <jinli.wang@mobvoi.com>
2018-12-18 20:08:26 +0800

    download scholar and makeup content

--- lesson_2/Reptile.py
+++ lesson_2/Reptile.py
@@ -1,16 +1,28 @@
+# -*- coding = utf-8 -*-
 import urllib.request
-# url_base = r'www.cnblogs.com'
-url_base = r'www.google.com.hk'
-HTTPS_PRE = r'https://'
-gs_url = HTTPS_PRE + url_base #+ r'/scholar?hl=en&as_sdt=0%2C5&as_ylo=2014&q=%22systematic+innovation%22&btnG='
+from bs4 import BeautifulSoup
 
-test_url = HTTPS_PRE + url_base + r'/kmonkeywyl/p/8458442.html'
-print(gs_url)
-headers = {'Host': url_base,
-           'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
-           'Accept': r'application/json, text/javascript, */*; q=0.01',
-           'Referer': HTTPS_PRE + url_base, }
-req = urllib.request.Request(gs_url, headers=headers)
-response = urllib.request.urlopen(req)
-html = response.read().decode('utf-8')
-print(html)
+
+def download_scholar_content(file_name):
+    url_base = r'scholar.google.com'
+    HTTPS_PRE = r'https://'
+    gs_url = HTTPS_PRE + url_base + \
+        r'/scholar?hl=en&as_sdt=0%2C5&as_ylo=2014&q=%22systematic+innovation%22&btnG='
+
+    headers = {'Host': url_base,
+               'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+               'Accept': r'application/json, text/javascript, */*; q=0.01',
+               'Referer': HTTPS_PRE + url_base, }
+    req = urllib.request.Request(gs_url, headers=headers)
+    response = urllib.request.urlopen(req)
+    html = response.read().decode('utf-8')
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    with open(file_name, "w", encoding='utf-8') as f:
+        f.writelines(soup.prettify())
+
+
+if __name__ == '__main__':
+    file_name = 'google_scholar.txt'
+    download_scholar_content(file_name)
